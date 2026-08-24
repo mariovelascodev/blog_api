3fb8bfe491ba829206babc5e32f076666f44d09f
Mario Velasco <105537753+mariovelascodev@users.noreply.github.com>
2026-08-24 16:53:09 +0200

    fix(db): activar autocommit en la conexión de aiomysql

--- src/blog_api/db/config.py
+++ src/blog_api/db/config.py
@@ -12,4 +12,5 @@ async def get_connection():
         user=os.getenv("MYSQL_USER"),
         password=os.getenv("MYSQL_PASSWORD"),
         db=os.getenv("MYSQL_DB"),
+        autocommit=True,
     )
